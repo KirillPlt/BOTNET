09154d88dd45fff4aeda65ca78eeb859cd0dd290
KasPer <158317202+KirillPlt@users.noreply.github.com>
2025-03-20 14:04:24 +0300

    Add files via upload

---
+++ BOTNET@Apathycal/config.py
@@ -0,0 +1,21 @@
+TOKEN = "7670171302:AAFF49Npk2V3LkkdQZ5y4BgLtDNFC1g2pbY" # Токен бота
+CRYPTO = "http://t.me/send?start=IVQ65awLYJ2W" # Токен CryptoBot
+ADMINS = [464283052,7980816641] # ID Админов
+
+# Bot Options
+bot_name = "PhantomBotnet"
+bot_logs = "-1002479396122"
+bot_channel_link = "https://t.me/Phantom BotnetChannel"
+bot_admin = "@Paralytical"
+bot_documentation = "https://t.me/+6HlAFBHSN0U2YmFh"
+bot_works = "https://t.me/+TotGXaVpymM0NmVh"
+bot_reviews = "https://t.me/+6BB7_IDbOTZhODNh"
+
+
+# Price
+subscribe_1_day = 1
+subscribe_7_days = 3
+subscribe_14_days = 8
+subscribe_30_days = 15
+subscribe_365_days = 50
+subscribe_infinity_days = 30

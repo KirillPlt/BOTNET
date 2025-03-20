ecb8fcdc420da4c7a89c2e5f3dc41b5042ad7ed5
KasPer <158317202+KirillPlt@users.noreply.github.com>
2025-03-20 14:06:56 +0300

    Update config.py

--- BOTNET@Apathycal/config.py
+++ BOTNET@Apathycal/config.py
@@ -1,15 +1,15 @@
-TOKEN = "7670171302:AAFF49Npk2V3LkkdQZ5y4BgLtDNFC1g2pbY" # Токен бота
-CRYPTO = "http://t.me/send?start=IVQ65awLYJ2W" # Токен CryptoBot
-ADMINS = [464283052,7980816641] # ID Админов
+TOKEN = # Токен бота
+CRYPTO = # Токен CryptoBot
+ADMINS = # ID Админов
 
 # Bot Options
-bot_name = "PhantomBotnet"
-bot_logs = "-1002479396122"
-bot_channel_link = "https://t.me/Phantom BotnetChannel"
-bot_admin = "@Paralytical"
-bot_documentation = "https://t.me/+6HlAFBHSN0U2YmFh"
-bot_works = "https://t.me/+TotGXaVpymM0NmVh"
-bot_reviews = "https://t.me/+6BB7_IDbOTZhODNh"
+bot_name = 
+bot_logs = 
+bot_channel_link = 
+bot_admin = 
+bot_documentation = 
+bot_works = 
+bot_reviews =
 
 
 # Price
